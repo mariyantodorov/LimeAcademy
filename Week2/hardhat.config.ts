0cd956379853c817b497c4ab1e87ac845e3d9237
Mariyan Todorov <mariyan.todorov13@gmail.com>
2022-10-12 08:53:57 +0300

    hardhat basic operation

---
+++ Week2/hardhat.config.ts
@@ -0,0 +1,19 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+
+const config: HardhatUserConfig = {
+  solidity: {
+    version: "0.8.17",
+    settings: { optimizer: { enabled: true, runs: 200 } },
+  },
+  networks: {
+    goerli: {
+      url: "https://goerli.infura.io/v3/199e48875a4f431e8ebaed6412865604",
+      accounts: [
+        "0x47c99abed3324a2707c28affff1267e45918ec8c3f20b8aa892e8b065d2942dd, 0x2a871d0798f97d79848a013d4936a73bf4cc922c825d33c1cf7073dff6d409c6",
+      ],
+    },
+  },
+};
+
+export default config;
