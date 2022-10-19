3d7f5b9e11ca05d4b71b096aacdc6dcfd07221d6
Mariyan Todorov <mariyan.todorov13@gmail.com>
2022-10-19 14:21:41 +0300

    added tests for events emiting

--- Week2-Task/test/BookLibrary.ts
+++ Week2-Task/test/BookLibrary.ts
@@ -15,87 +15,71 @@ describe("BookLibrary", function () {
     const BookLibrary = await ethers.getContractFactory("BookLibrary");
     const bookLibrary = await BookLibrary.deploy();
 
-    return { owner, otherAccount };
+    return { owner, otherAccount, bookLibrary };
   }
 
   describe("Deployment", function () {
-    // it("Should set the right unlockTime", async function () {
-    //   const { lock, unlockTime } = await loadFixture(deployBookLibraryFixture);
-    //   expect(await lock.unlockTime()).to.equal(unlockTime);
-    // });
-    // it("Should set the right owner", async function () {
-    //   const { lock, owner } = await loadFixture(deployOneYearLockFixture);
-    //   expect(await lock.owner()).to.equal(owner.address);
-    // });
-    // it("Should receive and store the funds to lock", async function () {
-    //   const { lock, lockedAmount } = await loadFixture(
-    //     deployOneYearLockFixture
-    //   );
-    //   expect(await ethers.provider.getBalance(lock.address)).to.equal(
-    //     lockedAmount
-    //   );
-    // });
-    // it("Should fail if the unlockTime is not in the future", async function () {
-    //   // We don't use the fixture here because we want a different deployment
-    //   const latestTime = await time.latest();
-    //   const Lock = await ethers.getContractFactory("Lock");
-    //   await expect(Lock.deploy(latestTime, { value: 1 })).to.be.revertedWith(
-    //     "Unlock time should be in the future"
-    //   );
-    // });
+    it("Should set the right owner", async function () {
+      const { bookLibrary, owner } = await loadFixture(
+        deployBookLibraryFixture
+      );
+      expect(await bookLibrary.owner()).to.equal(owner.address);
+    });
   });
 
-  describe("Withdrawals", function () {
-    // describe("Validations", function () {
-    //   it("Should revert with the right error if called too soon", async function () {
-    //     const { lock } = await loadFixture(deployOneYearLockFixture);
-    //     await expect(lock.withdraw()).to.be.revertedWith(
-    //       "You can't withdraw yet"
-    //     );
-    //   });
-    //   it("Should revert with the right error if called from another account", async function () {
-    //     const { lock, unlockTime, otherAccount } = await loadFixture(
-    //       deployOneYearLockFixture
-    //     );
-    //     // We can increase the time in Hardhat Network
-    //     await time.increaseTo(unlockTime);
-    //     // We use lock.connect() to send a transaction from another account
-    //     await expect(lock.connect(otherAccount).withdraw()).to.be.revertedWith(
-    //       "You aren't the owner"
-    //     );
-    //   });
-    //   it("Shouldn't fail if the unlockTime has arrived and the owner calls it", async function () {
-    //     const { lock, unlockTime } = await loadFixture(
-    //       deployOneYearLockFixture
-    //     );
-    //     // Transactions are sent using the first signer by default
-    //     await time.increaseTo(unlockTime);
-    //     await expect(lock.withdraw()).not.to.be.reverted;
-    //   });
-  });
+  describe("BookAdding", function () {});
+
+  describe("BookBorrow", function () {});
+
+  describe("BookReturn", function () {});
 
   describe("Events", function () {
-    //   it("Should emit an event on withdrawals", async function () {
-    //     const { lock, unlockTime, lockedAmount } = await loadFixture(
-    //       deployOneYearLockFixture
-    //     );
-    //     await time.increaseTo(unlockTime);
-    //     await expect(lock.withdraw())
-    //       .to.emit(lock, "Withdrawal")
-    //       .withArgs(lockedAmount, anyValue); // We accept any value as `when` arg
-    //   });
-  });
+    it("Should emit an event on book added", async function () {
+      const { bookLibrary } = await loadFixture(deployBookLibraryFixture);
+      const bookName = "TestBookName";
+      const bookCopies = 3;
 
-  describe("Transfers", function () {
-    //   it("Should transfer the funds to the owner", async function () {
-    //     const { lock, unlockTime, lockedAmount, owner } = await loadFixture(
-    //       deployOneYearLockFixture
-    //     );
-    //     await time.increaseTo(unlockTime);
-    //     await expect(lock.withdraw()).to.changeEtherBalances(
-    //       [owner, lock],
-    //       [lockedAmount, -lockedAmount]
-    //     );
-    //   });
+      await expect(bookLibrary.addNewBook(bookName, bookCopies))
+        .to.emit(bookLibrary, "LogAddedBook")
+        .withArgs(bookName, bookCopies);
+    });
+
+    it("Should emit an event on book borrowed", async function () {
+      const { bookLibrary, otherAccount } = await loadFixture(
+        deployBookLibraryFixture
+      );
+      const bookTitle = "Shogun";
+      //toUtf8Bytes because the addNewBook method uses abi.encodedPacked which returns uint8 encoding
+      //if abi.encode used in contract, which returns 32 bytes, utils.formatBytes32String will be needed
+      const bookTitleBytes32String = ethers.utils.toUtf8Bytes(bookTitle);
+      const bookTitleHashed = ethers.utils.keccak256(bookTitleBytes32String);
+
+      await bookLibrary.addNewBook(bookTitle, 3);
+
+      await expect(
+        bookLibrary.connect(otherAccount).borrowBook(bookTitleHashed)
+      )
+        .to.emit(bookLibrary, "BookBorrowed")
+        .withArgs(bookTitle, otherAccount.address);
+    });
+
+    it("Should emit an event on book returned", async function () {
+      const { bookLibrary, otherAccount } = await loadFixture(
+        deployBookLibraryFixture
+      );
+      const bookTitle = "Tai Pan";
+      //see notes in previous test
+      const bookTitleBytes32String = ethers.utils.toUtf8Bytes(bookTitle);
+      const bookTitleHashed = ethers.utils.keccak256(bookTitleBytes32String);
+
+      await bookLibrary.addNewBook(bookTitle, 3);
+      await bookLibrary.connect(otherAccount).borrowBook(bookTitleHashed);
+
+      await expect(
+        bookLibrary.connect(otherAccount).returnBook(bookTitleHashed)
+      )
+        .to.emit(bookLibrary, "BookReturned")
+        .withArgs(bookTitle, otherAccount.address);
+    });
   });
 });
