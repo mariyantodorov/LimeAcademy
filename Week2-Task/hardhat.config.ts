d9314754092303bed068563db381696c6924f9fe
Mariyan Todorov <mariyan.todorov13@gmail.com>
2022-10-19 12:13:52 +0300

    add etherscan verification

--- Week2-Task/hardhat.config.ts
+++ Week2-Task/hardhat.config.ts
@@ -13,8 +13,7 @@ const config: HardhatUserConfig = {
     goerli: {
       url: "https://goerli.infura.io/v3/199e48875a4f431e8ebaed6412865604",
       accounts: [
-        "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80",
-        "0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d",
+        "8b81e9caa6bf9213794c34c4a12b2fe85996a94bd4e0845659ba7325ddd3cdfc",
       ],
     },
   },
